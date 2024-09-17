e28e4f51ec87855673b265cf04591d8447b77b5b
Flash-ang <55903719+Flash-ang@users.noreply.github.com>
2024-09-17 22:10:55 +0800

    Create luainfo.lua
    test CPU and lua int 32 or 64 bit.

---
+++ test/luainfo.lua
@@ -0,0 +1,23 @@
+
+function _86or64()
+    if(0xfffffffff==0xffffffff) then return 32 else return 64 end
+end
+
+print( 'INT ', _86or64(), 'bit' );
+print( 'CPU ', #string.pack("T",0)*8, 'bit' )
+
+--[[
+
+D:\lua-5.4.7\x64>lua54-x64.exe luainfo.lua
+INT     64      bit
+CPU     64      bit
+
+D:\lua-5.4.7\x86>lua54-x86.exe luainfo.lua
+INT     64      bit
+CPU     32      bit
+
+
+note : 
+lua x86 and x64 both can run same luac file, IF both are using same "64" bit int.
+
+]]--
