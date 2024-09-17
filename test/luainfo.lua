0ecf146e4d33e0f282d88f6600ea43f8c32b0785
Flash-ang <55903719+Flash-ang@users.noreply.github.com>
2024-09-17 22:18:35 +0800

    Update luainfo.lua
    typo message.

--- test/luainfo.lua
+++ test/luainfo.lua
@@ -4,18 +4,17 @@ function _86or64()
 end
 
 print( 'INT ', _86or64(), 'bit' );
-print( 'CPU ', #string.pack("T",0)*8, 'bit' )
+print( 'compiler', #string.pack("T",0)*8, 'bit' )
 
 --[[
 
 D:\lua-5.4.7\x64>lua54-x64.exe luainfo.lua
 INT     64      bit
-CPU     64      bit
+compiler        64      bit
 
 D:\lua-5.4.7\x86>lua54-x86.exe luainfo.lua
 INT     64      bit
-CPU     32      bit
-
+compiler        32      bit
 
 note : 
 lua x86 and x64 both can run same luac file, IF both are using same "64" bit int.
